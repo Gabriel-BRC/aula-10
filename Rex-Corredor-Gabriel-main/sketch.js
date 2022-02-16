86c2b9b5014927fa7e3925cb3e97ea821c1f2608
Gabriel-BRC <99754689+Gabriel-BRC@users.noreply.github.com>
2022-02-16 17:46:45 -0300

    Aula 10
    16/02/22

---
+++ Rex-Corredor-Gabriel-main/sketch.js
@@ -0,0 +1,49 @@
+var rex, rexCorrendo;
+var terra ;
+var terra_plana
+
+
+function preload(){
+  
+  rexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
+ terra_plana = loadImage ("ground2.png");
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  rex = createSprite(50, 160, 20, 50);
+  rex.scale = 0.5;
+  rex.addAnimation("correndo", rexCorrendo);
+ 
+  borda = createEdgeSprites();
+terra = createSprite(200, 180, 400, 20) ;
+terra.addImage (terra_plana) ;
+terra.x = terra.width/2 ; 
+
+}
+
+function draw(){
+  background("white");
+
+  if(keyDown("space")){
+    rex.velocityY = -10;
+  }
+  rex.velocityY += 1;
+  terra.velocityX = -2; 
+
+if(terra.x < 0){
+terra.x = terra.width/2 ;
+
+
+
+
+
+}
+
+
+
+  rex.collide(terra);
+  
+  drawSprites();
+}
